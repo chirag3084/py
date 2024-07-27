41b37895be483e9e2be412c200b38e211ac6e570
Surti Chirag <chiragsurti308444@gmail.com>
2024-07-27 13:03:17 +0530

    first

---
+++ class&object.py
@@ -0,0 +1,36 @@
+# # # try:
+# # #     class pen:
+# # #         color = ["red","green"]
+# # #         price = [23,20,67,99]
+# # #         p1 = pen()
+# # #         print(p1.price)
+# # #         print(p1.price[0:2])
+# # # except:
+# # #     print("helllo")
+     
+#     #   try and except - 2
+
+# # # try:
+# # #     a = 4/0
+# # #     print(a)
+# # # except:
+# # #     print("correct it")
+
+# # class t:
+# #     a= 6
+# #     b = "oop"
+# # t1 = t()
+# # print(t1.a)
+
+class cars:
+    def parts():
+        a = 6 + 6
+p2 = cars()
+print(p2.parts())
+
+
+
+
+
+
+
